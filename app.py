39bf3ca00435b96975c8d11c6e47cc3f056e065b
Kshitij-Sharma49 <202052321@iiitvadodara.ac.in>
2023-06-02 20:52:37 +0530

    added cors

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import json
 import csv
 import os
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 # from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 import re
 import pandas as pd
@@ -16,6 +17,7 @@ from keras.models import load_model
 
 
 app = Flask(__name__)
+CORS(app)
 # api = Api(app)
 
 
